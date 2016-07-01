d839d901b434e312f8969a5167e3bb081a0091e8
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2016-07-01 15:21:48 +0200

    Working prototype for json file based check of input file metadata (standard_name, cell_methods and variable name)

--- icclim/metadata_database/metadata_database.py
+++ icclim/metadata_database/metadata_database.py
@@ -9,13 +9,15 @@ class VariableMetadata(object):
     Class to handle required variable meta data
 
     '''
-    def __init__(self, metadata_file, alt_names):
+    def __init__(self, metadata_file, alt_names, alt_methods):
         '''
         Initialize (read and parse) variable meta data json-file
 
         param metadata_file: path to variable metadata json file
         type metadata_file: string
         param alt_names: path to json file with alternative variable names
+        type metadata_file: string
+        param alt_methods: path to json file with alternative variable cell_methods
         type metadata_file: string
 
         '''
@@ -24,6 +26,9 @@ class VariableMetadata(object):
 
         falt_names = open(alt_names)
         self.jalt_names = json.load(falt_names)
+
+        falt_methods = open(alt_methods)
+        self.jalt_methods = json.load(falt_methods)
 
     def get_indice_dict(self, indice_name):
         '''
@@ -80,16 +85,65 @@ class VariableMetadata(object):
 
     def get_alternative_names(self, known_variable):
         '''
-        Fetches valid alternative variable names accoring to
+        Fetches valid alternative variable names according to
         ICCLIM variable meta data file.
 
         param known_variable: Default name for input variable for this index
-        type var_name: string
+        type known_variable: string
 
         rtype list
 
         '''
         return self.jalt_names['input']['known_variables'][known_variable]
+
+    def get_alternative_cmethods(self, known_method):
+        '''
+        Fetches valid alternative cell_methods according to
+        ICCLIM variable meta data file.
+
+        param known_method: Default name for input cell_method for this index
+        type known_method: string
+
+        rtype list
+
+        '''
+        return self.jalt_methods['input']['known_methods'][known_method]
+
+    def is_cell_method_consistent(self, VARS, indice_name):
+        '''
+        Checks whether cell method in the input files are consistent
+        with the current ICCLIM variable meta data file
+
+        param VARS: Main ICCLIM dictionary
+        type VARS: dict
+        param indice_name: User specified indice name
+        type var_name: string
+
+        rtype bool
+
+        '''
+        fcell_methods = []
+        for v in VARS.keys():
+            inc = Dataset(VARS[v]['files_years'].keys()[0], 'r')
+            ncVar = inc.variables[v]
+            fcell_methods.append(ncVar.cell_methods)
+
+        ret_value = True
+        curr_meta_dict = self.get_indice_dict(indice_name)
+        for idx in range(int(curr_meta_dict['n_inputs'])):
+            cell_methods = curr_meta_dict['input'][idx]['cell_methods']
+            alternative_cmethods = self.get_alternative_cmethods(cell_methods)
+
+            cell_methods = self.convert_to_lower_case_list(cell_methods)
+            union = set(alternative_cmethods) & set(fcell_methods)
+
+            if len(union) == 0:
+                ret_value = ret_value and False
+            else:
+                # Remove the matched entry
+                del(fcell_methods[fcell_methods.index(list(union)[0])])
+
+        return ret_value
 
     def is_standard_name_consistent(self, VARS, indice_name):
         '''
@@ -117,6 +171,9 @@ class VariableMetadata(object):
             jstandard_name = curr_meta_dict['input'][idx]['standard_name']
             if not jstandard_name in fstandard_names:
                 ret_value = ret_value and False
+            else:
+                # Remove the matched entry
+                del(fstandard_names[fstandard_names.index(jstandard_name)])
 
         return ret_value
 
@@ -138,15 +195,30 @@ class VariableMetadata(object):
         for idx in range(int(curr_meta_dict['n_inputs'])):
             known_variable = curr_meta_dict['input'][idx]['known_variables']
             alternative_names = self.get_alternative_names(known_variable)
-            if type(var_name) is list:
-                input_names = [name.lower() for name in var_name]
-            else:
-                input_names = [var_name.lower()]
-            union = set(alternative_names) & set(input_names)
+
+            var_name = self.convert_to_lower_case_list(var_name)
+            union = set(alternative_names) & set(var_name)
+
             if len(union) == 0:
                 ret_value = ret_value and False
 
         return ret_value
+
+    def convert_to_lower_case_list(self, invar):
+        '''
+        Outputs input variable as lower case list
+
+        param invar: User specified variable name
+        type invar: string
+
+        rtype list
+
+        '''
+        if type(invar) is list:
+            lc_list = [name.lower() for name in invar]
+        else:
+            lc_list = [invar.lower()]
+        return lc_list
 
 
 class GlobalMetadata(object):
